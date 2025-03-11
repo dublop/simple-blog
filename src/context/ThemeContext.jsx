f94110f2034adbd4a4768a65dca87ce9ea7219ff
dublop <dnlopezavila@gmail.com>
2025-03-11 14:16:43 -0400

    Add localstorage for theme data

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,23 +1,31 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState } from "react";
 
 // eslint-disable-next-line react-refresh/only-export-components
-export const ThemeContext = createContext()
+export const ThemeContext = createContext();
 
 export function ThemeProvider({ children }) {
-  const [theme, setTheme] = useState('light')
+  const localStorageTheme =
+    JSON.parse(localStorage.getItem("theme")) || "light";
+  const [theme, setTheme] = useState(localStorageTheme);
+
+  const updateLocalStorage = (value) => {
+    localStorage.setItem("theme", JSON.stringify(value));
+  };
 
   const handdleTheme = () => {
-    if (theme ==='light') {
-      const newValue = 'dark'
-      setTheme(newValue)
+    if (theme === "light") {
+      const newValue = "dark";
+      updateLocalStorage(newValue);
+      setTheme(newValue);
     } else {
-      const newValue = 'light'
-      setTheme(newValue)
+      const newValue = "light";
+      updateLocalStorage(newValue);
+      setTheme(newValue);
     }
-  }
+  };
   return (
     <ThemeContext.Provider value={{ theme, handdleTheme }}>
-        {children}
+      {children}
     </ThemeContext.Provider>
-  )
+  );
 }
